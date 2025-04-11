68fa9848fea3bdecddd95ca33d346a66f37b5a43
Sjoneon <spdjdps1649@gmail.com>
2025-04-11 13:37:10 +0900

    first commit and Push

---
+++ app/src/main/java/com/example/cap/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.example.cap;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final int SPLASH_DURATION = 2000; // 2초
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish(); // 스플래시 액티비티 종료
+            }
+        }, SPLASH_DURATION);
+    }
+}
